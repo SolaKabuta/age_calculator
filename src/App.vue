53af7f7f21b8f06ca126a4c81362fda06025d7bf
Sola <solakabuta@gmail.com>
2024-09-18 01:49:11 +0200

    feat: logical operation added

--- src/App.vue
+++ src/App.vue
@@ -17,13 +17,30 @@ console.log(`date ${day} / ${month} / ${year}`);
 const blank = "- -"
 
 
+const actualYear = computed(() => {
+  const actualYear = (year - yearData.value)
+
+  return actualYear
+})
+
+const actualMonth = computed(() => {
+  const actualMonth = (month - monthData.value)
+
+  return actualMonth
+})
+
+const actualDay = computed(() => {
+  const actualDay = (day - dayData.value)
+
+  return actualDay
+})
 
 </script>
 
 <template>
   <main class="bg-Off_white w-screen h-screen grid place-content-center font-bold">
     <section class="bg-white w-[800px] h-[600px] rounded-3xl rounded-br-[30%] px-10 py-10">
-      <div class="flex uppercase [&_div]:flex [&_div]:flex-col [&_input]:border [&_input]:border-Off_white [&_input]:px-6 [&_input]:py-2 [&_input]:my-2 [&_input]:w-3/4 [&_input]:rounded-md [&_input]:text-[32px]">
+      <div class="flex uppercase [&_label]:text-xs [&_div]:flex [&_div]:flex-col [&_input]:border [&_input]:border-Off_white [&_input]:px-6 [&_input]:py-2 [&_input]:my-2 [&_input]:w-3/4 [&_input]:rounded-md [&_input]:text-[32px]">
           <div>
             <label>Day</label>
             <input class="" type="text" v-model="dayData">
@@ -46,9 +63,9 @@ const blank = "- -"
 
       <!--RESULT-->
       <div class="[&_p]:text-8xl [&_p_span]:text-Purple [&_p_span]:px-2 [&_p]:text-black">
-      <p><span>{{ blank }}</span>years</p>
-      <p><span>{{ blank }}</span>months</p>
-      <p><span>{{ blank }}</span>days</p>
+      <p><span>{{ blank }}{{ actualYear }}</span>years</p>
+      <p><span>{{ blank }}{{ actualMonth }}</span>months</p>
+      <p><span>{{ blank }}{{ actualDay }}</span>days</p>
       </div>
 
     </section>
