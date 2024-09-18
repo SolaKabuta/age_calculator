53c8fda5b0dad8a6a185b4bbbdac4bbfb9a6b0b8
Sola <solakabuta@gmail.com>
2024-09-18 22:58:34 +0200

    style: layout improved with conditional rendering

--- src/App.vue
+++ src/App.vue
@@ -9,12 +9,14 @@ const monthData = ref('')
 const yearData = ref('')
 
 const now = new Date();
-const day = now.getDay(); // returns a number representing the day of the week, starting with 0 for Sunday
+const day = now.getDay() ; // returns a number representing the day of the week, starting with 0 for Sunday
 const month = now.getMonth() +1;
 const year = now.getFullYear();
 console.log(`date ${day} / ${month} / ${year}`);
 
 const isActive = ref(false)
+
+const isRequired = ref(false)
 
 const actualYear = ref('- -')
 const actualMonth = ref('- -')
@@ -23,27 +25,44 @@ const actualDay = ref('- -')
 
 function yearCount () {
   isActive.value = !isActive.value
-  actualYear.value = (year - yearData.value)
+  actualDay.value = (day - dayData.value)
   actualMonth.value = (month - monthData.value)
-  actualDay.value = (dayData.value - day)
+  actualYear.value = (year - yearData.value)
 }
 
-// function errorState () {
-//   if (isNaN(dayData.value)) {
-//     alert('test')
-//   }
-// }
+function stopCount () {
+  if (isActive.value = !isActive.value || isNaN(dayData.value) ) {
+    console.log('test not working') ;
+  } else {
+    console.log("test working")
+  }
+}
 
+function handleCLick () {
+  yearCount();
+  stopCount()
+
+}
+
+function errorState () {
+  let errorMessage = ref('not valid data')
+  if (dayData.value > 31 || dayData.value < 1) {
+    return errorMessage
+  }
+}
 
 </script>
 
 <template>
   <main class="bg-Off_white w-screen h-screen grid place-content-center font-bold">
-    <section class="bg-white w-[800px] h-[600px] rounded-3xl rounded-br-[30%] px-10 py-10">
+    <section class="bg-white w-[800px] h-[600px] rounded-3xl rounded-br-[30%] px-10 py-10" >
       <div class="flex uppercase [&_label]:text-xs [&_div]:flex [&_div]:flex-col [&_input]:border [&_input]:border-Off_white [&_input]:px-6 [&_input]:py-2 [&_input]:my-2 [&_input]:w-3/4 [&_input]:rounded-md [&_input]:text-[32px]">
           <div>
-            <label>Day</label>
+            <label :class="{'text-red-600' : isNaN(actualDay) && isActive,}">Day</label>
             <input  class="" type="text" v-model="dayData">
+            <transition name="fade">
+            <p class="text-xs text-red-600 transition duration-500 ease-out" v-if="isActive = !isActive">Must be a valid day</p>
+            </transition>
           </div>
           <div>
             <label>month</label>
@@ -56,9 +75,9 @@ function yearCount () {
       </div>
 
       <!--DIVIDER-->
-      <div class="flex">
+      <div class="flex" @keyup.enter="toggleActive">
         <div class="bg-Off_white w-full h-[1px] my-10"></div>
-        <button @click="yearCount"  :class="{'bg-Purple' : isActive, 'bg-black' : !isActive}" class="bg-Purple w-[90px] h-[80px] rounded-full grid place-content-center"><img src="@/assets/images/icon-arrow.svg" alt="arrow icon" width="46" height="44"></button>
+        <button @click="handleCLick"  :class="{'bg-Purple' : !isActive, 'bg-black' : isActive}" class="transition duration-500 ease-out w-[90px] h-[80px] rounded-full grid place-content-center"><img src="@/assets/images/icon-arrow.svg" alt="arrow icon" width="46" height="44"></button>
       </div>
 
       <!--RESULT-->
@@ -73,5 +92,11 @@ function yearCount () {
 </template>
 
 <style scoped>
-
+/* Transition classes */
+.fade-enter-active, .fade-leave-active {
+  transition: opacity 1s ease-in-out;
+}
+.fade-enter, .fade-leave-to {
+  opacity: 0;
+}
 </style>
